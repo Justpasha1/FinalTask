4369d2c70c09f16352ba3243ba2019bd72d1ea64
RubyCat <bmaksimyt@gmail.com>
2023-06-08 22:31:37 +0300

    add product

---
+++ auntification/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render,redirect 
+from .models import User
+from django.db.utils import IntegrityError
+
+
+# Create your views here.
+def show_registration(request):
+    context={}
+    response  = render(request, "auntificationapp/reg.html")
+    if request.method == "GET" and request.COOKIES.get('LogIn') is not None:
+        return redirect('../')
+    if request.method == "POST":
+        name=request.POST.get('name')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        phone=request.POST.get('phone')
+        email=request.POST.get('email')
+        users = User.objects.all()
+        for user in users:
+            if name != user.name:
+                if email != user.email:
+                    if phone != user.phone:
+                        UserNew = User(name=request.POST.get('name'),password=request.POST.get('password'),phone=request.POST.get('phone'),email=request.POST.get('email'))
+                        UserNew.save()
+                        response.set_cookie('LogIn', True)
+                        print('succes')
+                        response  = render(request, "auntificationapp/reg.html")
+                        return response
+                    else:
+                        context['error_text']= 'Phone number is already taken'
+                        print("phone")
+                        response  = render(request, "auntificationapp/reg.html", context)
+                        return response
+                else:
+                    context['error_text']= 'Email is already taken'
+                    print("email")
+                    response  = render(request, "auntificationapp/reg.html", context)
+                    return response
+            else:
+                context['error_text']= 'Nickname is already taken'
+                print("name")
+                response  = render(request, "auntificationapp/reg.html", context)
+                return response
+        else:
+            context['error_text']= 'Паролі не співпадають!'
+            response  = render(request, "auntificationapp/reg.html", context)
+            return response
+
+    return response 
+
+def show_login(request):
+    context={}
+    response = render(request, "auntificationapp/login.html")
+    if request.method == "GET" and request.COOKIES.get('LogIn') is not None:
+        return redirect('../')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        password = request.POST.get("password")
+        users = User.objects.all()
+        for user in users:
+            if password == user.password and name == user.name:
+                print('пройшло')
+                response.set_cookie('LogIn', True)
+                return response
+        else:
+            for user in users:
+                if name != user.name:
+                    print('password')
+                    response = render(request, "auntificationapp/login.html", context={'error_text' : 'You enter wrong nickname'})
+                    return response
+                else:
+                    if name == user.name and password != user.password:
+                        print('name')
+                        context['error_text']= 'You entered wrong password'
+                        response  = render(request, "auntificationapp/login.html", context)
+                        return response
+    return response
---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from .models import Product, Category, Comment
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+def show_catalog(request):
+    context = {"list_products": Product.objects.all(), 'additional_category': Category.objects.all()}
+    respose = render(request, "catalogapp/catalog.html", context)
+    return respose
+def show_product(request, product_pk):
+    
+    context = {"list_products": Product.objects.all()}
+    response = render(request, 'catalogapp/product.html', context)
+    return response
